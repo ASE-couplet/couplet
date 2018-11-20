aa1d28808ebb3126a1ac156fa2c19604c3beed77
Yufei Xu <v-yufxu@microsoft.com>
2018-11-20 21:23:16 +0800

    the flask like code

--- website/website_run.py
+++ website/website_run.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import os
 from flask import Flask, request, url_for, send_from_directory
+from flask import render_template
+from flask_bootstrap import Bootstrap
 from werkzeug import secure_filename
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
@@ -8,17 +10,7 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.getcwd()
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
-
-
-html = '''
-    <!DOCTYPE html>
-    <title>Upload File</title>
-    <h1>Photo Upload</h1>
-    <form method=post enctype=multipart/form-data>
-         <input type=file name=file>
-         <input type=submit value=upload>
-    </form>
-    '''
+Bootstrap(app)
 
 
 def allowed_file(filename):
@@ -32,17 +24,24 @@ def uploaded_file(filename):
                                filename)
 
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/index', methods=['GET', 'POST'])
 def upload_file():
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            file_url = url_for('uploaded_file', filename=filename)
-            s = "result"
-            return html + '<br><img src=' + file_url + '>' + '<p>' + s + '</p>'
-    return html
+            # file_url = url_for('uploaded_file', filename=filename)
+            # s = "result"
+            # filename = filename.split(".")[0]
+            return render_template("uploader.html", filename=filename)
+    return render_template("index.html")
+
+@app.route('/result/<filename>')
+def result(filename):
+    file_url = url_for('uploaded_file', filename=filename)
+    sentence = "show_result"
+    return render_template("result.html", file_url=file_url, sentence=sentence)
 
 
 if __name__ == '__main__':
