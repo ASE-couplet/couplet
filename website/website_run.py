87b8d3a10fe9f8cc564931ad15f018e764cb8c88
Yufei Xu (FA Talent) <v-yufxu@microsoft.com>
2018-11-26 20:31:39 +0800

    test

--- website/website_run.py
+++ website/website_run.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 import os
-from flask import Flask, request, url_for, send_from_directory
+from flask import Flask, request, url_for, send_from_directory, session
 from flask import render_template
 from flask_bootstrap import Bootstrap
+from flask_socketio import SocketIO, emit
 from werkzeug import secure_filename
+import logging
 
 from textImage import textImage
 
@@ -12,7 +14,9 @@ ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = os.getcwd()
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
+app.config['SECRET_KEY'] = 'secret!'
 Bootstrap(app)
+socketio = SocketIO(app)
 
 
 def allowed_file(filename):
@@ -36,8 +40,8 @@ def upload_file():
             # file_url = url_for('uploaded_file', filename=filename)
             # s = "result"
             # filename = filename.split(".")[0]
-            strs = "往后余生,风雪是你,平淡是你,清贫也是你\n荣华是你,心底温柔是你,目光所致,也是你"
-            filename = textImage(strs, filename, (0, 0, 0))
+            # strs = "往后余生,风雪是你,平淡是你,清贫也是你\n荣华是你,心底温柔是你,目光所致,也是你"
+            # filename = textImage(strs, filename, (0, 0, 0))
             return render_template("uploader.html", filename=filename)
     return render_template("index_new.html")
 
@@ -48,5 +52,19 @@ def result(filename):
     return render_template("result.html", file_url=file_url, sentence=sentence)
 
 
+@socketio.on('image_url')
+def process_msg(msg):
+    # image_url = msg['data']
+    logging.info("runnnnnnnnn")
+    print('ttest')
+
+    return "<html> hello </html>"
+    # os.system("python sleep.py")
+    # # read result
+    # strs = "往后余生,风雪是你,平淡是你,清贫也是你\n荣华是你,心底温柔是你,目光所致,也是你"
+    # filename = textImage(strs, filename, (0, 0, 0))
+    # return "url_for('/result/{}'.format(filename))"
+
 if __name__ == '__main__':
-    app.run()
+    print('test')
+    socketio.run(app, host='127.0.0.1')
